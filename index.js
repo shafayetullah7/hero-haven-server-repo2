30eda5c84b41c52420347954da4b29bc37b8a9e8
shafayetullah7 <shafayet769@gmail.com>
2023-05-20 16:36:31 +0600

    delete toy

--- index.js
+++ index.js
@@ -48,27 +48,72 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/all-toys',async(req,res)=>{
-      let query= {};
-      if(req.query?.email){
-        query={userEmail:req.query.email}
+    app.get('/all-toys', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query.userEmail = req.query.email;
+      } else if (req.query?.categoryLabel) {
+        query['category.label'] = req.query.categoryLabel;
       }
-      // console.log('query : ',query);
+      
       let result;
-      if(query?.userEmail){
-        result = await toys.find(query).toArray();
+      if (Object.keys(query).length > 0) {
+        result = await toys.find(query).limit(20).toArray();
+      } else {
+        result = await toys.find().limit(20).toArray();
       }
-      else{
-        result = await toys.find().toArray();
-      }
+      
       res.send(result || []);
-    })
+    });
+
+
+    app.get('/all-toys/:toyName', async (req, res) => {
+      try {
+        const toyName = req.params.toyName;
+        const regexPattern = new RegExp(toyName.replace(/[-.\s]/g, ''), 'i');
+        const query = { toyName: { $regex: regexPattern, $options: 'i' } };
+        const result = await toys.find(query).toArray();
+        res.send(result || []);
+      } catch (error) {
+        console.error('Error retrieving toys:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+    
+    
 
     app.get('/details/:id',async(req,res)=>{
       const id = req.params.id;
       const result = await toys.findOne({_id:new ObjectId(id)});
       res.send(result);
-    })
+    });
+
+
+    // Delete a document
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await toys.deleteOne({ _id:new ObjectId(id) });
+      console.log(result);
+      if (result.deletedCount === 1) {
+        res.send('Document deleted successfully');
+      } else {
+        res.status(404).send('Document not found');
+      }
+    });
+
+    // Update a document
+    // app.put('/toys/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //     const updates = req.body;
+    //     const result = await toys.updateOne({ _id:new ObjectId(id) }, { $set: updates });
+    //     if (result.matchedCount === 1) {
+    //       res.send('Document updated successfully');
+    //     } else {
+    //       res.status(404).send('Document not found');
+    //     }
+    // });
+
     
     
 
